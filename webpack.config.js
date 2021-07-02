e1b851a7cf44c29519c14bc00c74f93ce8665c16
조상호 컴퓨터 <teha007@naver.com>
2021-07-02 13:49:35 +0900

    웹팩완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
-  entry: './source/index.js',
+  entry: {
+    index: './source/index.js',
+    about: './source/about.js',
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'index_bundle.js',
+    filename: '[name]_bundle.js',
   },
   module: {
     rules: [
@@ -14,4 +18,16 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './source/index.html',
+      filename: './index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './source/about.html',
+      filename: './about.html',
+      chunks: ['about'],
+    }),
+  ],
 }
